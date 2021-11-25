ef6c5cd8717ba92565bd80c085572d54eb97b1bf
ChilliD <87444703+ChilliD@users.noreply.github.com>
2021-11-25 02:01:51 -0500

    nov pricesheet

---
+++ scripts.js
@@ -0,0 +1,78 @@
+let url = '/items.json';
+let prices = [];
+let container = document.getElementById('container');
+let searchbar = document.getElementById('search-bar');
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        prices = data;
+        //prices.forEach(item => createRow(item));
+        //createItemCard(prices[1]);
+    })
+
+searchbar.addEventListener('keyup', (e) => {
+    let str = e.target.value;
+
+    if (str.length === 0) {
+        hideContainer();
+    } else {
+        showContainer();
+        let resultBox = document.createElement('div');
+        let results = prices.filter(item => {
+            item.item.toLowerCase() == str.toLowerCase();
+        });
+
+        results.forEach(item => {
+            createItemCard(item);
+        });
+        
+        container.appendChild(resultBox);
+    }
+});
+
+function createRow(item) {
+    let row = document.createElement('div');
+    let name = item.item;
+    let cost = item.cost;
+    row.classList.add('row');
+    row.innerHTML = `${name}: ${cost}`;
+    container.appendChild(row);
+}
+
+function createItemCard(item) {
+    showContainer();
+    let card = document.createElement('div');
+    let name = item.item;
+    let cost = item.cost;
+    let pic = item.img;
+    let category = item.category;
+    let coin = '/images/gold_coin.png';
+
+    card.classList.add('item-card');
+    card.innerHTML = `
+        <div class="card-top">
+        <div class="card-text">
+        <span class="card-title">${name}</span><br />
+        <span class="card-category">${category}</span>
+        </div>
+        <img class="item-pic" src=${pic}><br />
+        </div>
+        <div class="card-bottom">
+        <span class="card-cost">${cost}
+        <img class="coin-pic" src="${coin}"></span>
+        </div>
+    `;
+
+    container.appendChild(card);
+}
+
+function hideContainer() {
+    container.style.visibility = 'hidden';
+}
+
+function showContainer() {
+    container.style.visibility = 'visible';
+}
+
+hideContainer();
